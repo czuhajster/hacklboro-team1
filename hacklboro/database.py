9edd05b30b5327f4f9065456eb60af621ca730e4
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-12 19:08:54 +0000

    Create generate_data for database.py

--- hacklboro/database.py
+++ hacklboro/database.py
@@ -16,3 +16,30 @@ if not os.path.isfile(DATABASE_FILE):
     con = sqlite3.connect(DATABASE_FILE)
     init_db()
     con.close()
+
+
+def generate_data():
+    import random
+    from hacklboro.auth import User
+    from hacklboro.goals import create_goal
+
+    with sqlite3.connect(DATABASE_FILE) as con:
+        for i in range(10):
+            con.execute(
+                """
+                INSERT INTO company (name, rating, text) VALUES (?, ?, ?)
+                """,
+                (
+                    f"Company {i}", random.randrange(0, 100),
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                    "Vestibulum et nibh erat. Cras at nulla metus."
+                )
+            )
+
+    for i in range(10):
+        User.create(f"User{i}", "password")
+
+    for i in range(10):
+        create_goal(i, random.random() * 100, f"Name {i}_1", random.random() > 0.5)
+        create_goal(i, random.random() * 100, f"Name {i}_2", random.random() > 0.5)
+        create_goal(i, random.random() * 100, f"Name {i}_3", random.random() > 0.5)
