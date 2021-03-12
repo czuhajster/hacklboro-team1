f188fed409a3e9b641da2ac0c23e9a72733afe7d
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-12 15:25:11 +0000

    Create get_goals in goals.py

--- hacklboro/goals.py
+++ hacklboro/goals.py
@@ -1,6 +1,9 @@
 import sqlite3
 
+from sqlite3 import Cursor
+
 from hacklboro.database import DATABASE_FILE
+from hacklboro.utilities import row_list_to_json
 
 
 def create_goal(user: int, percentage: float, name: str, increasing: bool) -> None:
@@ -19,3 +22,18 @@ def update_goal(id: int, percentage: float) -> None:
             "UPDATE goals SET percentage = ? WHERE id = ?",
             (percentage, id)
         )
+
+
+def get_goals(user: int, limit: int = 20) -> list[sqlite3.Row]:
+    with sqlite3.connect(DATABASE_FILE) as con:
+        con.row_factory = sqlite3.Row
+        cur: Cursor = con.execute(
+            "SELECT * FROM goals WHERE user = ? ORDER BY id DESC LIMIT ?",
+            (user, limit)
+        )
+        return cur.fetchall()
+
+
+def get_goals_as_json(user: int, limit: int = 20) -> str:
+    rows = get_goals(user, limit)
+    return row_list_to_json(rows)
