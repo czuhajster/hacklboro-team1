4e024eee8ed453675bab8c9c41a65d4943a1bb89
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-12 13:28:17 +0000

    Create base files for project

---
+++ hacklboro/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from flask_login import LoginManager
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # TODO: Do login
+        pass
+    else:
+        return render_template("home.html")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
