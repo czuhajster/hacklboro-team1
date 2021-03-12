9fa9bec4f2e98afaf0c6349a5a2afcccdfd4ad0c
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-12 15:24:05 +0000

    Add row_list_to_json to utilities.py

--- hacklboro/utilities.py
+++ hacklboro/utilities.py
@@ -8,3 +8,11 @@ def row_to_json(row: sqlite3.Row) -> str:
         d[key] = row[key]
 
     return json.dumps(d)
+
+
+def row_list_to_json(rows: list[sqlite3.Row]) -> str:
+    l = []
+    for row in rows:
+        l.append(row_to_json(row))
+
+    return json.dumps(l)
