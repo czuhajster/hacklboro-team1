6ae636bf3a92d885831a3e169ac6c821e150b821
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-12 14:13:48 +0000

    Create sqlite_schema for when database is missing

--- hacklboro/database.py
+++ hacklboro/database.py
@@ -1,5 +1,21 @@
+import os
 import sqlite3
+from sqlite3 import Connection
 
 DATABASE_FILE = "hacklboro/database.db"
+SCHEMA_FILE = "hacklboro/sqlite_schema"
 
-con = sqlite3.connect(DATABASE_FILE)
+
+def init_db():
+    with open(SCHEMA_FILE) as f:
+        text = f.read()
+        con.executescript(text)
+        con.commit()
+
+
+if not os.path.isfile(DATABASE_FILE):
+    con = sqlite3.connect(DATABASE_FILE)
+    init_db()
+    con.close()
+
+con: Connection = sqlite3.connect(DATABASE_FILE)
