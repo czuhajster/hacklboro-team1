4bb4e4c09a39625981a2f7a61d4ab97ba15593ea
czuhajster <18arek@gmail.com>
2021-03-13 19:56:29 +0100

    Add not found message functionality.

--- hacklboro/search.js
+++ hacklboro/search.js
@@ -1,33 +1,57 @@
 // Implement search funcionality for traffic lights page.
 
-$(document).ready(function() {
-  var names = [];
+
+// Find company and show error message if not found.
+
+function findCompany(name) {
+  for (let i = 0; i < companies.length; i++) {
+    if (name == companies[i].innerHTML) {
+      window.location.hash = '#' + name;
+      $("#" + name).focus();
+      $(".not-found").remove();
+      return; // Terminate function
+    }
+  }
+  if ($(".not-found").length == 0) {
+    $(".search-bar").append("<div class='not-found'>Company not found</div>");
+  }
+}
+
+
+// Make list of all companies loaded from databse.
+
+function addCompanies() {
   var companies = $(".company-name");
   for (let i = 0; i < companies.length; i++) {
     names.push(companies[i].innerHTML);
   }
+  return companies;
+}
 
-  // Add auto completion
+
+// Execute when document is ready.
+
+$(document).ready(function() {
+  var names = [];
+  var companies = addCompanies();
+
+  // Add auto completion.
 
   $(".search-bar input").autocomplete({
     source: names,
-    appendTo: ".search-bar > ul"
-  });
-  $(".search-bar input").keyup(function (event) {
-    if (event.key == "Enter") {
-      focusCard($(".search-bar input").val());
+    appendTo: ".search-bar > ul",
+    open: function () {
+      if($(".not-found").length == 1) {
+        $(".not-found").remove();
+      }
     }
   });
 
-  function focusCard(name) {
-    for (let i = 0; i < companies.length; i++) {
-      if (name == companies[i].innerHTML) {
-        window.location.hash = '#' + name;
-        $("#" + name).focus();
-        return; // Terminate function
-      }
+  // Search for company when Enter is pressed.
+
+  $(".search-bar input").keyup(function (event) {
+    if (event.key == "Enter") {
+      findCompany($(".search-bar input").val());
     }
-    alert("Company \"" + name + "\" has not been found");
-    $(".search-bar ul").append("<li class='not-found'>Company not found</li>");
-  }
+  });
 });
