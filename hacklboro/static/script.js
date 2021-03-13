1627de3330a6d2665d2b359b567c7a2c226115e2
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-03-13 16:58:33 +0000

    Create goal suggestions in goals page

--- hacklboro/static/script.js
+++ hacklboro/static/script.js
@@ -1,11 +1,6 @@
-// Function to update the percentage of a user's goal by sending a PUT request to the server
-function changePercentage(id, percentage) {
+// Function to send form data to the server via JavaScript
+function sendForm(data, method) {
     const XHR = new XMLHttpRequest();
-
-    const data = {
-        id: id,
-        percentage: percentage
-    };
 
     let urlEncodedData = "",
         urlEncodedDataPairs = [],
@@ -22,7 +17,38 @@ function changePercentage(id, percentage) {
     XHR.addEventListener('error', function (event) {
         alert('Oops! Something went wrong.');
     });
-    XHR.open('PUT', '/goals/data');
+    XHR.open(method, '/goals/data');
     XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     XHR.send(urlEncodedData);
+}
+
+// Function to update the percentage of a user's goal by sending a PUT request to the server
+function changePercentage(id, percentage) {
+    const data = {
+        id: id,
+        percentage: percentage
+    };
+    sendForm(data, "PUT");
+}
+
+// Suggest goal based on the dropdown given
+function goalSuggestion(i) {
+    let goalText = "";
+    switch (i) {
+        case 0:
+            goalText = "Reduce meat consumption (at most 3 meat-based meals this week)";
+            break;
+        case 1:
+            goalText = "Reduce car emissions (drive at most 5 times this week)";
+            break;
+        case 2:
+            goalText = "Use environmentally friendly transport (use bicycle at least 5 times this week)";
+            break;
+    }
+
+    const data = {
+        name: goalText
+    };
+
+    sendForm(data, "POST");
 }
